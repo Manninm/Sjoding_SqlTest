b0ccfaaf0c80fc3932aa6dfa85108ec93b5e64b4
Matthew <manninm@umich.edu>
2019-12-12 11:32:31 -0500

    Downloaded CSV files necessary to make MIMICIII Demo Database

---
+++ Database/1-define.sql
@@ -0,0 +1,5 @@
+SQLite version 3.28.0 2019-04-16 19:49:53
+Enter ".help" for usage hints.
+Connected to a [1mtransient in-memory database[0m.
+Use ".open FILENAME" to reopen on a persistent database.
+sqlite> ^C^C^C
